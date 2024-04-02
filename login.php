c8b80fff3d7ce6ec51c4bcbb63dcec81b55c5428
miyashd3 <3miya.10dai@gmail.com>
2024-04-03 07:11:49 +0900

    completed loginpage

--- login.php
+++ login.php
@@ -3,16 +3,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="loginstyle.css">
   <title>login</title>
 </head>
 <body>
-  <h1>emailでログイン</h1>
-  <form action="">
-    <input type="email" name="email" placeholder="email">
-    <input type="password" name="password" placeholder="password">
-    <input type="submit" value="ログイン">
-  </form>
-  <h2>SNSでログイン</h2>
+  
+  <fieldset>
+    <h1>ログイン</h1>
+    <form>
+      <div class="iconUser"></div>
+        <input type="text" placeholder="email" required>
+          <div class="iconPassword"></div>
+        <input type="password" placeholder="Password" required>
+        <input type="submit" value="Enter">
+    </form>
+  </fieldset>
 </body>
 </html>
 

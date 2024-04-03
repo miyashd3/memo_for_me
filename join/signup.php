341e6453187a22bcba873431032411119cf0d8cd
miyashd3 <3miya.10dai@gmail.com>
2024-04-03 15:05:32 +0900

    completed signup_page

---
+++ join/signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="login_signup_style.css">
+  <title>会員登録</title>
+</head>
+<body>
+  
+  <fieldset>
+    <h1>会員登録</h1>
+    <form>
+        <div class="iconUser"></div>
+        <input type="text" placeholder="email" required>
+        <p class="error">* メールアドレスを入力してください</p>
+        <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
+        <div class="iconPassword"></div>
+        <input type="password" placeholder="Password" required>
+        <p class="error">* パスワードを入力してください</p>
+        <p class="error">* パスワードは4文字以上で入力してください</p>
+        <input type="submit" value="登録する">
+    </form>
+  </fieldset>
+</body>
+</html>

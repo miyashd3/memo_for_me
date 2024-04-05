798556d6c0b27187c96a5e0632f4c5905db025fb
miyashd3 <3miya.10dai@gmail.com>
2024-04-06 07:09:03 +0900

    update

---
+++ userpage/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>あなたのメモ</title>
+  <link rel="stylesheet" href="../index_style.css">
+</head>
+<body>
+  <header>
+    <p>〇〇さんのページ</p>
+    <button class="header">ログアウト</button>
+  </header>
+  <div class="menu">
+    <a href="new_memo.php"><button>新規追加</button></a>
+    <form action="検索結果を処理するページのURL" method="get">
+      <input type="text" name="search" placeholder="検索...">
+      <button type="submit">検索</button>
+    </form>
+  </div>
+
+  <hr>
+
+  <div class="list">
+    <h2>メモ一覧</h2>
+    <ul>
+      <a href=""><li>memoタイトル</li></a>
+      <a href=""><li>memoです</li></a>
+      <a href=""><li>memoです</li></a>
+    </ul>
+  </div>
+</body>
+</html>

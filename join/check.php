e09a27ef2f2c1d774db30e5ad7c7ae65f28d7dd1
miyashd3 <3miya.10dai@gmail.com>
2024-04-13 14:21:12 +0900

    commit php first

--- join/check.php
+++ join/check.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+require('../library.php');
+
+if (isset($_SESSION['form'])) {
+	$form = $_SESSION['form'];
+} else {
+	header('Location: signup.php');
+	exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$db = dbconnect();
+	if (!$db) {
+		die($db->error);
+	}
+
+	$stmt = $db->prepare('insert into users (name, email, password) VALUES (?, ?, ?)');
+	if (!$stmt) {
+		die($db->error);
+	}
+	$password = password_hash($form['password'], PASSWORD_DEFAULT);
+	$stmt->bind_param('sss', $form['name'], $form['email'], $password);
+	$success = $stmt->execute();
+	if (!$success) {
+		die($db->error);
+	}
+
+	unset($_SESSION['form']);
+	header('Location: entry_comp.php');
+}
+?>
 <!-- 入力内容の確認 -->
 <!DOCTYPE html>
 <html lang="ja">
@@ -16,14 +48,15 @@
 		<div id="content">
 			<p>記入した内容を確認して、「登録する」ボタンをクリックしてください</p>
 			<form action="" method="post">
-				<dl>
-					<p>名前</p>
-					<p></p>
-					<p>メールアドレス</p>
-					<p></p>
-					<p>パスワード</p>
-					<p>【表示されません】</p>
-				<div><a href="">訂正する</a> | <input type="submit" value="登録する" /></div>
+				<p>名前</p>
+				<p><?php echo h($form['name']); ?></p>
+				<p>メールアドレス</p>
+				<p><?php echo h($form['email']); ?></p>
+				<p>パスワード</p>
+				<p>【表示されません】</p>
+				<div>
+					<a href="signup.php? action=rewrite">訂正する</a> | <input type="submit" value="登録する" />
+				</div>
 			</form>
 		</div>
 
--- join/login.php
+++ join/login.php
@@ -1,3 +1,43 @@
+<?php
+require('../library.php');
+session_start();
+
+$error = [];
+$email = '';
+$password = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  if ($email === '' || $password === '') {
+    $error['login'] = 'blank';
+  } else {
+    $db = dbconnect();
+    $stmt = $db->prepare('select id, name, password from users where email=? limit 1');
+    if (!$stmt) {
+      die($db->error);
+    }
+
+    $stmt->bind_param('s', $email);
+    $success = $stmt->execute();
+    if (!$success) {
+      die($db->error);
+    }
+
+    $stmt->bind_result($id, $name, $hash);
+    $stmt->fetch();
+
+    if (password_verify($password, $hash)) {
+      session_regenerate_id();
+      $_SESSION['id'] = $id;
+      $_SESSION['name'] = $name;
+      header('Location: ../userpage/index.php');
+      exit();
+    } else {
+      $error['login'] = 'failed';
+    }
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,13 +50,17 @@
   
   <fieldset>
     <h1>ログイン</h1>
-    <form>
+    <form action="" method="post">
         <div class="iconUser"></div>
-        <input type="text" placeholder="email" required>
+        <input type="text" name="email" size="35" maxlength="255" placeholder="email" required value="<?php echo h($email); ?>">
         <div class="iconPassword"></div>
-        <input type="password" placeholder="Password" required>
-        <p class="error">* メールアドレスとパスワードをご記入ください</p>
-        <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+        <input type="password" name="password" size="35" maxlength="255" placeholder="Password" required">
+        <?php if (isset($error['login']) && $error['login'] === 'blank'): ?>
+          <p class="error">* ログインに失敗しました。正しくご記入ください。</p>
+        <?php endif; ?>
+        <?php if (isset($error['login']) && $error === 'failed'): ?>
+          <p class="error">* メールアドレスとパスワードをご記入ください</p>
+        <?php endif; ?>
         <input type="submit" value="Enter">
     </form>
   </fieldset>

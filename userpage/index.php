ab741922f00ee4e31f44317cb6fc82125cceba38
miyashd3 <3miya.10dai@gmail.com>
2024-04-20 06:35:24 +0900

    completed Displayed

--- userpage/index.php
+++ userpage/index.php
@@ -28,7 +28,7 @@ $db = dbconnect();
   <div class="menu">
     <a href="new_memo.php"><button>新規追加</button></a>
     <form action="検索結果を処理するページのURL" method="get">
-      <input type="text" name="search" placeholder="検索...">
+      <input type="text" name="search" placeholder="検索...(未実装)">
       <button type="submit">検索</button>
     </form>
   </div>
@@ -38,11 +38,20 @@ $db = dbconnect();
   <div class="list">
     <h2>メモ一覧</h2>
     <?php
-    $stmt = $db->prepare('select id, users_id, title, memo, created from memos where users_id=? order by id desc');
+    #　最大ページ数を求める
+    $counts = $db->query('select count(*) as cnt from memos');
+    $count = $counts->fetch_assoc();
+    $max_page = floor(($count['cnt']+1) / 5+1);
+
+    $stmt = $db->prepare('select id, users_id, title, memo, created from memos where users_id=? order by id desc limit ?, 5');
     if (!$stmt) {
       die($db->error);
     }
-    $stmt->bind_param('i', $id);
+    $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_NUMBER_INT);
+    $page = ($page ?: 1);
+    $start = ($page - 1) * 5;
+
+    $stmt->bind_param('ii', $id, $start);
     $success = $stmt->execute();
     if (!$success) {
       die($db->error);
@@ -58,6 +67,12 @@ $db = dbconnect();
         </div>
       </ul>
     <?php endwhile; ?>
+    <?php if ($page > 1): ?>
+    <p><a href="index.php?page=<?php echo $page - 1; ?>">前のページ</a></p>
+    <?php endif; ?>
+    <?php if ($page<$max_page): ?>
+      <p><a href="index.php?page=<?php echo $page + 1; ?>">次のページ</a></p>
+    <?php endif; ?>
   </div>
 </body>
 </html>
